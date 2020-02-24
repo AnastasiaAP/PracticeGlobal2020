7ff747575c8e8915c171b9927117207e54fa20fb
AnastasiaAP <nakapyndzyn@gmail.com>
2020-02-24 18:45:43 +0200

    file from entity

---
+++ EntityDB/EntityDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
